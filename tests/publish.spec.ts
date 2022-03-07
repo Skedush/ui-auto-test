ed2a99ea3e9841a7bdf53f324bf9fe78eb781847
望君(软件研究所) <uid19046@sansi.com>
2022-03-07 15:33:33 +0800

    test(publish): ui-auto-test#4 排期页面：新增测试

--- tests/publish.spec.ts
+++ tests/publish.spec.ts
@@ -12,16 +12,16 @@ test('测试手动发布-节目库中的第一个节目有内容', async ({ page
     // Click button:has-text("添加节目")
     await page.locator('button:has-text("添加节目")').click();
     // expect first label-link exist
-    await expect(page.locator('.table-link').first()).toBeDefined()
+    await expect(page.locator('.table-link').first()).toBeVisible()
     // expect first radio exist
-    await expect(page.locator('input[type="radio"]').first()).toBeDefined()
+    await expect(page.locator('input[type="radio"]').first()).toBeVisible()
 });
 
 test('测试手动发布-播放器库中的第一个播放器有内容', async ({ page }) => {
     // Click button:has-text("添加播放器")
     await page.locator('button:has-text("添加播放器")').click();
     // expect first span exist
-    await expect(page.locator('label').first()).toBeDefined()
+    await expect(page.locator('label').first()).toBeVisible()
     await expect(page.locator('label').first()).toHaveText('修改时间:')
 
   });
